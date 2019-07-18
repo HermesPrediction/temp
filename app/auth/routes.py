d1fe367a20df4c6d8333bb5199ad5797f88202c2
Larry Bliss <LarryBliss@Larrys-MBP.home>
2019-07-18 14:14:24 +0100

    heroku deployment changes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -13,22 +13,24 @@ import itertools
 import random
 
 
+
 # The deafult method is GET, if we only needed this when we can just write @bp.route('/login')
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
     form = LoginForm()
-    user = User.query.filter_by(username=form.username.data).first()
-    if user is None or not user.check_password(form.password.data):
-        flash('Invalid username or password')
-        return redirect(url_for('auth.login'))
-    login_user(user, remember=form.remember_me.data)
-    next_page = request.args.get('next')
-    if not next_page or url_parse(next_page).netloc != '':
-        next_page = url_for('main.index')
-    return redirect(next_page)
-    # return render_template('auth/login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.index')
+        return redirect(next_page)
+    return render_template('auth/login.html', title='Sign In', form=form)
 
 
 @bp.route('/logout')
